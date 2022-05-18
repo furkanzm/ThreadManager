060f7f5cfdfeff7b20fa44c52746225b1972f71c
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-18 16:36:36 +0300

    Update main.cpp

--- ThreadManangment/main.cpp
+++ ThreadManangment/main.cpp
@@ -1,25 +1,31 @@
 #include "threadmanager.h"
 
+int i = 0;
 int run()
 {
-	int i = 0;
-	std::cout << " Asking Worker " << std::endl;
-	if (i++ > 10)
+
+	i += 1;
+	std::cout << "Asking Worker to Stop  " << i << std::endl;
+
+	if (i > 10)
 		return -1;
+
 	return 0;
 }
 
 int main()
 {
-
 	ThreadManager work;
 	work.setFunc(run);
 	work.setInterval(1); // saniye
 	work.start();
-	//work.wait();
-	//work.stop();
-	//work.notify();
-    
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // milisaniye
+	// work.wait();
+
+	// work.notify();
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(500)); // milisaniye
+	// work.stop();
+	work.stop();
+	std::this_thread::sleep_for(std::chrono::milliseconds(4000)); // milisaniye
 	return 0;
 }
