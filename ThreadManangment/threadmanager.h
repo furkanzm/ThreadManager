a4bc4fc05c6bbb1c59803835822f0859c7ffb7c6
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-18 16:35:51 +0300

    Update threadmanager.h

--- ThreadManangment/threadmanager.h
+++ ThreadManangment/threadmanager.h
@@ -1,30 +1,37 @@
-#include <assert.h>
 #include <chrono>
-#include <functional>
-#include <future>
-#include <iostream>
-#include <thread>
-#include <mutex>
 #include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <thread>
 
+using namespace std::chrono_literals;
 
 class ThreadManager
 {
 public:
-    int loopTime = 0;    
-    int ctrl = 0;    
-    ThreadManager();
-    ~ThreadManager();
-    std::thread *thread_object;
-    void start(); // başlayacak
-    void stop();  // duracak, kullanıcı istediğinide de durdurabilecek.
-    void setFunc(std::function<int(void)> cb); // iş görecek funksiyon
-    void setInterval(int x);				   // ne kadar çalışacak
-    void wait();   // kendi kendine bitmesini bekleyeceğiz
-    void notify(); // tekrar başlama
-    bool sFlag;
-    void worker();
-    void lock();
-    void unlock();
-    std::function<int(void)> cb;
+    
+	ThreadManager();
+	~ThreadManager();
+	void start();							// başlayacak
+	void stop(); // duracak, kullanıcı istediğinidede durdurabilecek.
+	void setFunc(std::function<int(void)> cb); // iş görecek funksiyon
+	void setInterval(int x);				   // ne kadar çalışacak
+	void wait();   // kendi kendine bitmesini bekleyeceğiz
+	void notify(); // tekrar başlama
+    void spin();
+private:
+    int i ;
+    int buffer = 0;
+	int miliSecond = 100;
+	std::function<int()> cb;
+	void worker();
+	std::thread *thread_object;
+	std::mutex mtx;
+	std::condition_variable stoper;
+    std::unique_lock<std::mutex> lock{ mtx };    
+	bool stopped = false;
+	bool sFlag;
+  //  int i = 0;
+
 };
