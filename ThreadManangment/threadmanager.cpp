19d31a323adb6c73e8e4acf3c34eaa646328696a
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-18 16:34:28 +0300

    Update threadmanager.cpp

--- ThreadManangment/threadmanager.cpp
+++ ThreadManangment/threadmanager.cpp
@@ -1,68 +1,76 @@
 #include "threadmanager.h"
-#include <mutex>
-std::mutex mtx;
-std::condition_variable stoper;
-bool stopped = false;
 
 ThreadManager::ThreadManager()
 {
-     
 }
 
 ThreadManager::~ThreadManager()
 {
+	delete thread_object;
 }
 
 void ThreadManager::start()
 {
-   
+	// std::lock_guard<std::mutex> guard(mtx);
+	// startTime = std::chrono::steady_clock::now();
 	sFlag = true;
-	std::cout << "Thread Start" << std::endl;
 	thread_object = new std::thread(&ThreadManager::worker, this);
-    
+
 } // başlayacak
 
 void ThreadManager::setFunc(std::function<int(void)> cb)
 {
-
 	this->cb = cb;
 } // iş görecek funksiyon
 
 void ThreadManager::setInterval(int x)
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(x));
+	miliSecond *= x;
 
 } // ne kadar çalışacak
 
 void ThreadManager::wait()
 {
-	std::unique_lock<std::mutex> L {mtx};
+	/*std::unique_lock<std::mutex> L {mtx};
 	stoper.wait(L, [&]() { return stopped; });
 	if (stopped) {
 		std::cout << "wait!" << std::endl;
-	}
-
+	}*/
+	thread_object->join();
 } // kendi kendine bitmesini bekleyeceğiz
 
 void ThreadManager::notify()
 {
-	std::lock_guard<std::mutex> G(mtx);
-	std::cout << "notify" << std::endl;
-	stoper.notify_one();
+	std::unique_lock<std::mutex> lck(mtx);
+	while (stoper.wait_for(lck, std::chrono::seconds(miliSecond)) ==
+		   std::cv_status::timeout) {
+		std::cout << '.' << std::endl;
+	}
 } // tekrar başlama
 
 void ThreadManager::stop()
 {
-	sFlag = true;
-	std::cout << "Thread End" << std::endl;
+	sFlag = false;
+	stoper.notify_one();
+	std::cout << "thread exit" << std::endl;
+
 } // duracak, kullanıcı istediğinide de durdurabilecek.
+void ThreadManager::spin()
+{
+	const int work = 1000 * 1000 * 40;
+	volatile int v = 0;
+	for (int j = 0; j < work; ++j)
+		++v;
+}
 
 void ThreadManager::worker()
 {
 	while (sFlag) {
-        
-        cb();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        
+
+		int temp = cb();
+		if (temp)
+			break;
+		spin();
+		// lck.unlock();
 	}
 }
